80ac70123366c5f1948da62ac4a22be3b60dad7e
Esteban Budis <estbeban.budis@epitech.eu>
2019-10-18 17:57:18 +0200

    Test for my get nbr

--- test_my_getnbr.c
+++ test_my_getnbr.c
@@ -4,27 +4,27 @@ int my_getnbr(char const *str);
 
 Test(my_getnbr, should_return_42)
 {
-    char str[] = "jemepresentejemappellehenry42gentillepersonne";
+    char str[] = "42jemepresentejemappellehenry42gentillepersonne";
     int nb = 0;
 
-    nb = my_getnbr(str);
+    nb = my_getnbr("42jemepresentejemappellehenry42gentillepersonne");
     cr_assert_eq(nb, 42);
 }
 
 Test(my_getnbr, should_return_neg42)
 {
-    char str[] = "jemepresentejemappelle---henry-42gentillepersonne";
+    char str[] = "-42";
     int nb = 0;
 
-    nb = my_getnbr(str);
+    nb = my_getnbr("-42");
     cr_assert_eq(nb, -42);
 }
 
 Test(my_getnbr, should_return_0)
 {
-    char str[] = "jemepresenteppellehenry42123456789876543234567854";
+    char str[] = "76543234567854";
     int nb = 0;
 
-    nb = my_getnbr(str);
+    nb = my_getnbr("76543234567854");
     cr_assert_eq(nb, 0);
 }
