6fc6b1fcdf72fcbad69bab75aa5fb03238a808e8
Léo Sarochar <56728067+LeoSarochar@users.noreply.github.com>
2019-10-18 17:47:12 +0200

    Merging Karim's tests and student's tests

--- CPool_WorkshopLib_Tests/test_my_revstr.c
+++ CPool_WorkshopLib_Tests/test_my_revstr.c
@@ -2,15 +2,15 @@
 
 char *my_revstr(char *str);
 
-Test(my_revstr, return_empty_str)
+Test(my_revstr, should_return_empty_str)
 {
     char str[] = "";
+    
     my_revstr(str);
-
-    cr_assert("");
+    cr_assert_str_empty(str);
 }
 
-Test(my_revstr, return_hello_revstr)
+Test(my_revstr, should_return_hello_revstr)
 {
     char str[] = "Hello";
     
@@ -18,10 +18,31 @@ Test(my_revstr, return_hello_revstr)
     cr_assert("olleH");
 }
 
-Test(my_revstr, return_helloworld_revstr)
+Test(my_revstr, should_return_helloworld_revstr)
 {
     char str[] = "Hello World !";
 
     my_revstr(str);
     cr_assert("! dlroW olleH");
+}
+
+Test(my_revstr, should_return_string_HELLO)
+{
+    char message[] = "HELLO";
+    my_revstr(message);
+    cr_assert_str_eq(message, "OLLEH");
+}
+
+Test(my_revstr, should_return_string_Z)
+{
+    char message[] = "Z";
+    my_revstr(message);
+    cr_assert_str_eq(message, "Z");
+}
+
+Test(my_revstr, should_return_string_TOTO)
+{
+    char message[] = "TOTO";
+    my_revstr(message);
+    cr_assert_str_eq(message, "OTOT");
 }
